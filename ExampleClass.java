1011e148741a37b947fd24ee112aa7f2beb20df6
Ramkumar03387 <65547033+Ramkumar03387@users.noreply.github.com>
2021-05-07 16:04:11 +0530

    Update ExampleClass.java

--- ExampleClass.java
+++ ExampleClass.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class ExampleClass {
+public class FindTotalAndPer{
 	
 	    public static void main(String[] args) {
 			int n = 5;
 			float total = 0f;
-			for(int i=1;i<=n;i++) {
+			for(int i=1;i<=n;i++)
+			{
 				Scanner sc = new Scanner(System.in);
-		    	System.out.println("enter the mark in subject: "+i);
-		    	int mark = sc.nextInt();
+		    		System.out.println("enter the mark in subject: "+i);
+		    		int mark = sc.nextInt();
 				total = mark+total;
 			}
 			System.out.println(total/n+"%");
